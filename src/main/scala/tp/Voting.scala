0c0da97476e4945605c6ff1afa97e890f6e8ab94
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2018-11-07 18:09:25 -0300

    Finish Voting implementation

--- src/main/scala/tp/Voting.scala
+++ src/main/scala/tp/Voting.scala
@@ -1,4 +1,4 @@
-package tp.chat
+package tp
 
 import scala.io.Source
 
@@ -6,20 +6,31 @@ import scala.io.Source
   *
   * @author Tomas Perez Molina
   */
-object Voting extends App {
+object VotingFuncs {
   case class VotingResult(state: String, district: String, candidate: String, votes: Int)
 
-  def loadResults(fileName: String): Array[VotingResult] = {
+  def loadResults(fileName: String): List[VotingResult] =
     Source.fromFile(fileName).getLines().map(_.split(",")).map {
       case Array(state, district, candidate, votes) => VotingResult(state, district, candidate, votes.toInt)
-    }
-  }
+    }.toList
 
   def totalVotes(results: List[VotingResult]): Int = results.map(_.votes).sum
 
   def winner(results: List[VotingResult]): String =
-    results.groupBy(_.candidate).map(t => (t._1, totalVotes(t._2))).maxBy(_._2)._1
+    results.groupBy(_.candidate).map { case (c, rs) => (c, totalVotes(rs)) }.maxBy(_._2)._1
 
   def bestState(results: List[VotingResult], candidate: String): String =
     results.filter(_.candidate == candidate).maxBy(_.votes).state
+
+}
+
+object Voting extends App {
+  import VotingFuncs._
+
+  private val results: List[VotingResult] = loadResults("voting.csv")
+  println(winner(results))
+  println(totalVotes(results))
+  println(bestState(results, "A"))
+  println(bestState(results, "B"))
+  println(bestState(results, "C"))
 }
